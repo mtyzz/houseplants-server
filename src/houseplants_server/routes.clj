b5369ae83bdeebda25a19613eb70578b03de26c2
mllehrer <mia.lehrer@gmail.com>
2020-10-13 21:39:39 -0700

    move handlers, refactor

--- src/houseplants_server/routes.clj
+++ src/houseplants_server/routes.clj
@@ -1,2 +1,19 @@
-(ns houseplants-server.routes)
+(ns houseplants-server.routes
+  (:require [houseplants-server.handlers :as h]))
 
+(def app-routes
+  ["/health" {:get (fn [req]
+                     {:status 200
+                      :body "ok"})}])
+
+(def plants-routes
+  ["/plants"
+   ["/" {:get h/get-all-plants}]
+   ["/:id" {:parameters {:path {:id int?}}
+            :get h/get-plant-by-id}]
+   ["/name/:name" {:parameters {:path {:name string?}}
+                   :get h/get-plants-by-name}]
+   ["/cadence/:cadence" {:parameters {:path {:cadence int?}}
+                         :get h/get-plant-by-cadence}]
+   ["/light/:light" {:parameters {:path {:light string?}}
+                     :get h/get-plants-by-light}]])
