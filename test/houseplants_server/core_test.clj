edd620075450f41fec1af2e84d89e55c8c4b0373
mllehrer <mia.lehrer@gmail.com>
2020-10-14 17:59:42 -0700

    Add basic route tests

---
+++ test/houseplants_server/core_test.clj
@@ -0,0 +1,19 @@
+(ns houseplants-server.core-test
+  (:require [houseplants-server.core :refer :all]
+            [clojure.test :refer :all]
+            [ring.mock.request :as mock]))
+
+(deftest test-app
+  (testing "main route redirect"
+    (let [response (app (mock/request :get "/"))]
+      (is (= (:status response) 302))
+      (is (= (:body response) ""))))
+
+  (testing "health check"
+    (let [response (app (mock/request :get "/health"))]
+      (is (= (:status response) 200))
+      (is (= (:body response) "ok"))))
+
+  (testing "not-found route"
+    (let [response (app (mock/request :get "/invalid"))]
+      (is (= (:status response) 404)))))
